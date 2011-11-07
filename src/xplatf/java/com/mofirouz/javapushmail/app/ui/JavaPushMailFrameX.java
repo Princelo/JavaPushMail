9b06ac7145c40f23dd13ad3ada7729e9aff38fa4
Alberto Colombo <alberto.colombo@flextrade.com>
2011-11-07 10:13:25 +0000

    build improvements
     * Required java 1.6
     * Conditionally include correct JavaPushMailFrameX based on OS

---
+++ src/xplatf/java/com/mofirouz/javapushmail/app/ui/JavaPushMailFrameX.java
@@ -0,0 +1,10 @@
+package com.mofirouz.javapushmail.app.ui;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Mo Firouz
+ * @since 15/10/11
+ */
+public class JavaPushMailFrameX extends JavaPushMailFrame {}
